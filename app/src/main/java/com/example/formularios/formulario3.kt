cc035d70c9b0ee67051bce46bc3f0a2dc13e7269
javi <javilopezguadix03@gmail.com>
2023-12-04 16:19:12 +0100

    commit

--- app/src/main/java/com/example/formularios/formulario3.kt
+++ app/src/main/java/com/example/formularios/formulario3.kt
@@ -6,38 +6,29 @@ import android.os.Bundle
 import android.widget.Button
 import com.google.android.material.textfield.TextInputEditText
 
-class formulario2_main : AppCompatActivity() {
+class formulario3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.formulario2)
+        setContentView(R.layout.activity_formulario3)
 
-        var btnEnviar = findViewById<Button>(R.id.btnEnviar)
-        var etNombre = findViewById<TextInputEditText>(R.id.textInputEditTextNombre)
         var etCorreo = findViewById<TextInputEditText>(R.id.textInputEditTextCorreo)
         var etContra = findViewById<TextInputEditText>(R.id.textInputEditTextContra)
+        var etNombre = findViewById<TextInputEditText>(R.id.textInputEditTextNombre)
+        var etCP = findViewById<TextInputEditText>(R.id.textInputEditTextCP)
+        var etFecha = findViewById<TextInputEditText>(R.id.textInputEditTextFecha)
+        var btnEnviar = findViewById<Button>(R.id.btnEnviar)
 
         btnEnviar.setOnClickListener{
+            var bool1 = validarCorreo(etCorreo)
+            var bool2 = validarContra(etContra)
 
-            var bool1 = validarNombre(etNombre)
-            var bool2 = validarCorreo(etCorreo)
-            var bool3 = validarContra(etContra)
-
-            if(bool1 && bool2 && bool3){
+            if(bool1 && bool2){
                 var intent = Intent(this, formulario2_2::class.java)
                 startActivity(intent)
             }
         }
-
-
     }
-    fun validarNombre(etNombre:TextInputEditText):Boolean{
-        var esValido = true
-        if(etNombre.text.isNullOrBlank()){
-            etNombre.error = "Este campo no puede estar vacío"
-            esValido = false
-        }
-        return esValido
-    }
+
     fun validarCorreo(etCorreo:TextInputEditText):Boolean{
         var esValido = true
         if(etCorreo.text!!.contains("@") && etCorreo.text!!.contains(".")){
@@ -47,10 +38,11 @@ class formulario2_main : AppCompatActivity() {
         }
         return esValido
     }
+
     fun validarContra(etContra:TextInputEditText):Boolean{
         var esValido = true
-        if(etContra.text!!.length<6){
-            etContra.error = "La contraseña debe tener más de 6 carácteres"
+        if(etContra.text!!.length<7){
+            etContra.error = "Debe tener más de 7 carácteres, contener un número, una mayúscula y una minúscula"
             esValido = false
         }
         return esValido
